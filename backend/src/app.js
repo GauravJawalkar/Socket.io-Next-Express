f7a3eb2a07c4c07bf6ffe4589415098efc768d22
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-01-24 22:47:11 +0530

    hostupdate

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 const server = http.createServer(app);
 const PORT = 3001;
-const HOSTNAME = "localhost";
+const HOSTNAME = "https://express-next-chat-backend.vercel.app/";
 
 
 const io = new Server(server, {
